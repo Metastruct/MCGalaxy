a824888a19f15140cae0d2eeafd40b8918113052
SpiralP <spiral.palmer@gmail.com>
2019-04-16 13:57:02 -0700

    formatting

--- MCGalaxy/Commands/Command.Helpers.cs
+++ MCGalaxy/Commands/Command.Helpers.cs
@@ -5,10 +5,10 @@
     the GNU General Public License, Version 3 (the "Licenses"); you may
     not use this file except in compliance with the Licenses. You may
     obtain a copy of the Licenses at
-    
+
     http://www.opensource.org/licenses/ecl2.php
     http://www.gnu.org/licenses/gpl-3.0.html
-    
+
     Unless required by applicable law or agreed to in writing,
     software distributed under the Licenses are distributed on an "AS IS"
     BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
@@ -21,7 +21,7 @@ using MCGalaxy.Eco;
 using MCGalaxy.Games;
 
 namespace MCGalaxy {
-    
+
     public abstract partial class Command {
 
         const CommandEnable bothFlags = CommandEnable.Lava | CommandEnable.Zombie;
@@ -29,7 +29,7 @@ namespace MCGalaxy {
             if (enable == CommandEnable.Always) return null;
             if (enable == CommandEnable.Economy && !Economy.Enabled)
                 return "economy is disabled.";
-            
+
             if (enable == bothFlags && !(ZSGame.Instance.Running || LSGame.Instance.Running))
                 return "neither zombie nor lava survival is running.";
             if (enable == CommandEnable.Zombie && !ZSGame.Instance.Running)
@@ -38,77 +38,87 @@ namespace MCGalaxy {
                 return "lava survival is not running.";
             return null;
         }
-        
+
         protected bool CheckSuper(Player p, string message, string type) {
             if (message.Length > 0 || !p.IsSuper) return false;
             SuperRequiresArgs(name, p, type);
             return true;
         }
-        
+
         protected void SuperRequiresArgs(Player p, string type) { SuperRequiresArgs(name, p, type); }
-        
+
         protected internal static void SuperRequiresArgs(string cmd, Player p, string type) {
             p.Message("When using /{0} from {2}, you must provide a {1}.", cmd, type, p.SuperName);
         }
-        
+
         protected bool HasExtraPerm(Player p, LevelPermission plRank, int num) {
             return CommandExtraPerms.Find(name, num).UsableBy(plRank);
         }
-        
+
         protected bool CheckExtraPerm(Player p, CommandData data, int num) {
             if (HasExtraPerm(p, data.Rank, num)) return true;
-            
+
             CommandExtraPerms perms = CommandExtraPerms.Find(name, num);
             perms.MessageCannotUse(p);
             return false;
         }
-        
-        protected internal static bool CheckRank(Player p, CommandData data, Player who, 
-                                                 string action, bool canAffectOwnRank) {
+
+        protected internal static bool CheckRank(
+            Player p,
+            CommandData data,
+            Player who,
+            string action,
+            bool canAffectOwnRank
+        ) {
             return p == who || CheckRank(p, data, who.Rank, action, canAffectOwnRank);
         }
-        
-        protected internal static bool CheckRank(Player p, CommandData data, LevelPermission rank, 
-                                                 string action, bool canAffectOwnRank) {
+
+        protected internal static bool CheckRank(
+            Player p,
+            CommandData data,
+            LevelPermission rank,
+            string action,
+            bool canAffectOwnRank
+        ) {
             if (canAffectOwnRank && rank <= data.Rank) return true;
             if (!canAffectOwnRank && rank < data.Rank) return true;
-            
+
             if (canAffectOwnRank)
                 p.Message("Can only {0} players ranked {1} %Sor below", action, p.group.ColoredName);
             else
                 p.Message("Can only {0} players ranked below {1}", action, p.group.ColoredName);
             return false;
         }
-        
+
         protected static bool IsListModifier(string str) {
             int ignored;
             return str.CaselessEq("all") || int.TryParse(str, out ignored);
         }
-        
-        
+
+
         protected internal static bool IsCreateCommand(string str) {
             return str.CaselessEq("create") || str.CaselessEq("add") || str.CaselessEq("new");
-        } 
-        
+        }
+
         protected internal static bool IsDeleteCommand(string str) {
             return str.CaselessEq("del") || str.CaselessEq("delete") || str.CaselessEq("remove");
         }
-        
+
         protected internal static bool IsEditCommand(string str) {
-            return str.CaselessEq("edit") || str.CaselessEq("change") || str.CaselessEq("modify")
-                || str.CaselessEq("move") || str.CaselessEq("update");
-        }  
+            return str.CaselessEq("edit") || str.CaselessEq("change") || str.CaselessEq("modify") ||
+                str.CaselessEq("move") || str.CaselessEq("update");
+        }
 
         protected internal static bool IsInfoCommand(string str) {
-            return str.CaselessEq("about") || str.CaselessEq("info") || str.CaselessEq("status")
-                || str.CaselessEq("check");
+            return str.CaselessEq("about") || str.CaselessEq("info") || str.CaselessEq("status") ||
+                str.CaselessEq("check");
         }
-        
+
         protected internal static bool IsListCommand(string str) {
             return str.CaselessEq("list") || str.CaselessEq("view");
         }
     }
-    
+
     public sealed class CommandTypes {
         public const string Building = "build";
         public const string Chat = "chat";
