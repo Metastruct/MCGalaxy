b414c83234f926e38f34b8022386eb5d70cf0d05
SpiralP <spiral.palmer@gmail.com>
2019-04-16 15:09:16 -0700

    formatting

--- MCGalaxy/Commands/other/CmdBack.cs
+++ MCGalaxy/Commands/other/CmdBack.cs
@@ -1,14 +1,14 @@
 /*
     Copyright 2010 MCSharp team (Modified for use with MCZall/MCLawl/MCGalaxy)
-    
+
     Dual-licensed under the Educational Community License, Version 2.0 and
     the GNU General Public License, Version 3 (the "Licenses"); you may
     not use this file except in compliance with the Licenses. You may
     obtain a copy of the Licenses at
-    
+
     http://www.opensource.org/licenses/ecl2.php
     http://www.gnu.org/licenses/gpl-3.0.html
-    
+
     Unless required by applicable law or agreed to in writing,
     software distributed under the Licenses are distributed on an "AS IS"
     BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
@@ -23,14 +23,17 @@ namespace MCGalaxy.Commands.Misc {
 
         public override void Use(Player p, string message, CommandData data) {
             if (p.PreTeleportMap == null) {
-                p.Message("You have not teleported anywhere yet"); return;
+                p.Message("You have not teleported anywhere yet");
+                return;
             }
-            
-            if (!p.level.name.CaselessEq(p.PreTeleportMap))
+
+            if (!p.level.name.CaselessEq(p.PreTeleportMap)) {
                 PlayerActions.ChangeMap(p, p.PreTeleportMap);
+            }
+
             p.SendPos(Entities.SelfID, p.PreTeleportPos, p.PreTeleportRot);
         }
-        
+
         public override void Help(Player p) {
             p.Message("%T/Back");
             p.Message("%HTakes you back to the position you were in before teleportation");
