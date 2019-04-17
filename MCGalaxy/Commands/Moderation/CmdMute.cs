e2b34d5824a523363b2037506901777f21f30976
SpiralP <spiral.palmer@gmail.com>
2019-04-16 19:57:16 -0700

    formatting

--- MCGalaxy/Commands/Moderation/CmdMute.cs
+++ MCGalaxy/Commands/Moderation/CmdMute.cs
@@ -1,14 +1,14 @@
 /*
     Copyright 2011 MCForge
-        
+
     Dual-licensed under the Educational Community License, Version 2.0 and
     the GNU General Public License, Version 3 (the "Licenses"); you may
     not use this file except in compliance with the Licenses. You may
     obtain a copy of the Licenses at
-    
+
     http://www.opensource.org/licenses/ecl2.php
     http://www.gnu.org/licenses/gpl-3.0.html
-    
+
     Unless required by applicable law or agreed to in writing,
     software distributed under the Licenses are distributed on an "AS IS"
     BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
@@ -28,7 +28,7 @@ namespace MCGalaxy.Commands.Moderation {
         public override void Use(Player p, string message, CommandData data) {
             if (message.Length == 0) { Help(p); return; }
             string[] args = message.SplitSpaces(3);
-            
+
             Player who = PlayerInfo.FindMatches(p, args[0]);
             if (who == null) {
                 if (Server.muted.Contains(args[0])) Unmute(p, args[0], args);
@@ -40,27 +40,27 @@ namespace MCGalaxy.Commands.Moderation {
             } else {
                 Group group = ModActionCmd.CheckTarget(p, data, "mute", who.name);
                 if (group == null) return;
-                
+
                 TimeSpan duration = Server.Config.ChatSpamMuteTime;
                 if (args.Length > 1) {
                     if (!CommandParser.GetTimespan(p, args[1], ref duration, "mute for", "s")) return;
                 }
-                
+
                 string reason = args.Length > 2 ? args[2] : "";
                 reason = ModActionCmd.ExpandReason(p, reason);
                 if (reason == null) return;
-                
+
                 ModAction action = new ModAction(who.name, p, ModActionType.Muted, reason, duration);
                 OnModActionEvent.Call(action);
             }
         }
-        
+
         static void Unmute(Player p, string name, string[] args) {
             string reason = args.Length > 1 ? args[1] : "";
             reason = ModActionCmd.ExpandReason(p, reason);
-            if (reason == null) return;            
+            if (reason == null) return;
             if (p.name == name) { p.Message("You cannot unmute yourself."); return; }
-            
+
             ModAction action = new ModAction(name, p, ModActionType.Unmuted, reason);
             OnModActionEvent.Call(action);
         }
