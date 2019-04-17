f96d504c9f36cb322bf6e286e4bdcc4360837767
SpiralP <spiral.palmer@gmail.com>
2019-04-16 20:04:57 -0700

    formatting

--- MCGalaxy/Commands/Moderation/CmdTempRank.cs
+++ MCGalaxy/Commands/Moderation/CmdTempRank.cs
@@ -23,13 +23,13 @@ namespace MCGalaxy.Commands.Moderation {
         public override string type { get { return CommandTypes.Moderation; } }
         public override LevelPermission defaultRank { get { return LevelPermission.Operator; } }
         public override CommandAlias[] Aliases {
-            get { return new[] { new CommandAlias("dtr", "delete"), new CommandAlias("trl", "list") }; }
+            get { return new [] { new CommandAlias("dtr", "delete"), new CommandAlias("trl", "list") }; }
         }
-        
+
         public override void Use(Player p, string message, CommandData data) {
             string[] args = message.SplitSpaces(4);
             string cmd = args[0];
-            
+
             if (args.Length >= 3) {
                 Assign(p, args, data);
             } else if (IsListCommand(cmd)) {
@@ -42,7 +42,7 @@ namespace MCGalaxy.Commands.Moderation {
                 Help(p);
             }
         }
-        
+
         static void Assign(Player p, string[] args, CommandData data) {
             string target = PlayerInfo.FindMatchesPreferOnline(p, args[0]);
             if (target == null) return;
@@ -53,56 +53,58 @@ namespace MCGalaxy.Commands.Moderation {
             if (!CommandParser.GetTimespan(p, args[2], ref duration, "temp rank for", "h")) return;
 
             if (Server.tempRanks.Contains(target)) {
-                p.Message("%WThe player already has a temporary rank assigned!"); return;
+                p.Message("%WThe player already has a temporary rank assigned!");
+                return;
             }
-            
+
             if (p.name.CaselessEq(target)) {
-                p.Message("%WYou cannot assign yourself a temporary rank."); return;
+                p.Message("%WYou cannot assign yourself a temporary rank.");
+                return;
             }
-            
+
             Group curRank = PlayerInfo.GetGroup(target);
             string reason = args.Length > 3 ? args[3] : "assigning temp rank";
             if (!CmdSetRank.CanChangeRank(target, curRank, newRank, p, data, ref reason)) return;
-            
+
             ModAction action = new ModAction(target, p, ModActionType.Rank, reason, duration);
             action.targetGroup = curRank;
             action.Metadata = newRank;
             OnModActionEvent.Call(action);
         }
-        
+
         internal static void Delete(Player p, string target, CommandData data) {
             string line = Server.tempRanks.FindData(target);
             if (line == null) {
                 p.Message("{0} %Whas not been assigned a temp rank.",
-                          PlayerInfo.GetColoredName(p, target));
+                    PlayerInfo.GetColoredName(p, target));
                 return;
             }
-            
+
             string[] parts = line.SplitSpaces();
             Group curRank = PlayerInfo.GetGroup(target);
-            
+
             Group oldRank = Group.Find(parts[4 - 1]); // -1 because data, not whole line
             if (oldRank == null) return;
-            
+
             string reason = "temp rank unassigned";
             if (!CmdSetRank.CanChangeRank(target, curRank, oldRank, p, data, ref reason)) return;
-            
+
             ModAction action = new ModAction(target, p, ModActionType.Rank, reason);
             action.Metadata = oldRank;
             action.targetGroup = curRank;
             OnModActionEvent.Call(action);
         }
-        
+
         static void Info(Player p, string target) {
             string data = Server.tempRanks.FindData(target);
             if (data == null) {
                 p.Message("{0} %Whas not been assigned a temp rank.",
-                               PlayerInfo.GetColoredName(p, target));
+                    PlayerInfo.GetColoredName(p, target));
             } else {
                 PrintTempRankInfo(p, target, data);
             }
         }
-        
+
         static void List(Player p) {
             List<string> lines = Server.tempRanks.AllLines();
             if (lines.Count == 0) {
@@ -115,26 +117,26 @@ namespace MCGalaxy.Commands.Moderation {
                 }
             }
         }
-        
+
         static void PrintTempRankInfo(Player p, string name, string data) {
             string[] args = data.SplitSpaces();
             if (args.Length < 4) return;
-            
+
             string assigner = args[0];
             DateTime assigned = long.Parse(args[1]).FromUnixTime();
-            DateTime expiry   = long.Parse(args[2]).FromUnixTime();
-            string oldRank    = Group.GetColoredName(args[3]);
-            string tempRank   = Group.GetColoredName(args[4]);
-            
+            DateTime expiry = long.Parse(args[2]).FromUnixTime();
+            string oldRank = Group.GetColoredName(args[3]);
+            string tempRank = Group.GetColoredName(args[4]);
+
             TimeSpan assignDelta = DateTime.UtcNow - assigned;
             TimeSpan expireDelta = expiry - DateTime.UtcNow;
             p.Message("Temp rank information for {0}:",
-                           PlayerInfo.GetColoredName(p, name));
+                PlayerInfo.GetColoredName(p, name));
             p.Message("  From {0} %Sto {1}%S, by {2} &a{3} %Sago, expires in &a{4}",
-                           oldRank, tempRank, assigner,
-                           assignDelta.Shorten(), expireDelta.Shorten());
+                oldRank, tempRank, assigner,
+                assignDelta.Shorten(), expireDelta.Shorten());
         }
-        
+
         public override void Help(Player p) {
             p.Message("%T/TempRank [player] [rank] [timespan] <reason>");
             p.Message("%HSets a temporary rank for the specified player.");
--- MCGalaxy/Commands/Moderation/CmdUnbanip.cs
+++ MCGalaxy/Commands/Moderation/CmdUnbanip.cs
@@ -1,14 +1,14 @@
 /*
     Copyright 2010 MCSharp team (Modified for use with MCZall/MCLawl/MCGalaxy)
-    
+
     Dual-licensed under the Educational Community License, Version 2.0 and
     the GNU General Public License, Version 3 (the "Licenses"); you may
     not use this file except in compliance with the Licenses. You may
     obtain a copy of the Licenses at
-    
+
     http://www.opensource.org/licenses/ecl2.php
     http://www.gnu.org/licenses/gpl-3.0.html
-    
+
     Unless required by applicable law or agreed to in writing,
     software distributed under the Licenses are distributed on an "AS IS"
     BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
@@ -38,16 +38,16 @@ namespace MCGalaxy.Commands.Moderation {
             if (!IPAddress.TryParse(addr, out ip)) { p.Message("\"{0}\" is not a valid IP.", addr); return; }
             if (p.ip == addr) { p.Message("You cannot un-IP ban yourself."); return; }
             if (!Server.bannedIP.Contains(addr)) { p.Message(addr + " is not a banned IP."); return; }
-            
+
             string reason = args.Length > 1 ? args[1] : "";
             reason = ModActionCmd.ExpandReason(p, reason);
             if (reason == null) return;
-            
+
             ModAction action = new ModAction(addr, p, ModActionType.UnbanIP, reason);
             OnModActionEvent.Call(action);
         }
-        
-        public override void Help(Player p)  {
+
+        public override void Help(Player p) {
             p.Message("%T/UnbanIP [ip/player]");
             p.Message("%HUn-bans an IP, or the IP the given player is on.");
         }
