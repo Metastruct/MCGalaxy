44f90e2a906b872a9ebf346a30fcd415faab7345
SpiralP <spiral.palmer@gmail.com>
2019-04-16 20:04:57 -0700

    formatting

--- MCGalaxy/Commands/Moderation/CmdBanip.cs
+++ MCGalaxy/Commands/Moderation/CmdBanip.cs
@@ -1,14 +1,14 @@
 /*
     Copyright 2010 MCSharp team (Modified for use with MCZall/MCLawl/MCGalaxy)
-    
+
     Dual-licensed under the Educational Community License, Version 2.0 and
     the GNU General Public License, Version 3 (the "Licenses"); you may
     not use this file except in compliance with the Licenses. You may
     obtain a copy of the Licenses at
-    
+
     http://www.opensource.org/licenses/ecl2.php
     http://www.gnu.org/licenses/gpl-3.0.html
-    
+
     Unless required by applicable law or agreed to in writing,
     software distributed under the Licenses are distributed on an "AS IS"
     BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
@@ -43,34 +43,34 @@ namespace MCGalaxy.Commands.Moderation {
             if (Server.bannedIP.Contains(addr)) { p.Message("{0} is already IP banned.", addr); return; }
             // Check if IP is shared by any other higher ranked accounts
             if (!CheckIP(p, data, addr)) return;
-            
+
             string reason = args.Length > 1 ? args[1] : "";
             reason = ModActionCmd.ExpandReason(p, reason);
             if (reason == null) return;
-            
+
             ModAction action = new ModAction(addr, p, ModActionType.BanIP, reason);
             OnModActionEvent.Call(action);
         }
-        
+
         static bool CheckIP(Player p, CommandData data, string ip) {
             if (p.IsConsole) return true;
             List<string> accounts = PlayerInfo.FindAccounts(ip);
             if (accounts == null || accounts.Count == 0) return true;
-            
+
             foreach (string name in accounts) {
                 Group grp = PlayerInfo.GetGroup(name);
                 if (grp.Permission < data.Rank) continue;
-                
+
                 p.Message("You can only IP ban IPs used by players with a lower rank.");
                 p.Message(name + "(" + grp.ColoredName + "%S) uses that IP.");
-                
-                Logger.Log(LogType.SuspiciousActivity, 
-                           "{0} failed to ipban {1} - IP is also used by: {2}({3})", p.name, ip, name, grp.Name);
+
+                Logger.Log(LogType.SuspiciousActivity,
+                    "{0} failed to ipban {1} - IP is also used by: {2}({3})", p.name, ip, name, grp.Name);
                 return false;
             }
             return true;
         }
-        
+
         public override void Help(Player p) {
             p.Message("%T/BanIP [ip/player] <reason>");
             p.Message("%HBans an IP, or the IP the given player is on.");
--- MCGalaxy/Commands/Moderation/CmdFollow.cs
+++ MCGalaxy/Commands/Moderation/CmdFollow.cs
@@ -1,14 +1,14 @@
 /*
     Copyright 2011 MCForge
-    
+
     Dual-licensed under the Educational Community License, Version 2.0 and
     the GNU General Public License, Version 3 (the "Licenses"); you may
     not use this file except in compliance with the Licenses. You may
     obtain a copy of the Licenses at
-    
+
     http://www.opensource.org/licenses/ecl2.php
     http://www.gnu.org/licenses/gpl-3.0.html
-    
+
     Unless required by applicable law or agreed to in writing,
     software distributed under the Licenses are distributed on an "AS IS"
     BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
@@ -28,30 +28,29 @@ namespace MCGalaxy.Commands.Moderation {
             if (!p.canBuild) { p.Message("You're currently being &4possessed%S!"); return; }
             string[] args = message.SplitSpaces(2);
             string name = args[0];
-            
+
             bool stealth = false;
             if (message == "#") {
-                if (p.following.Length > 0) { stealth = true; name = ""; }
-                else { Help(p); return; }
+                if (p.following.Length > 0) { stealth = true; name = ""; } else { Help(p); return; }
             } else if (args.Length > 1 && args[0] == "#") {
                 if (p.hidden) stealth = true;
                 name = args[1];
             }
-            
-            if (name.Length == 0 && p.following.Length == 0) { Help(p); return; }            
+
+            if (name.Length == 0 && p.following.Length == 0) { Help(p); return; }
             if (name.CaselessEq(p.following) || (name.Length == 0 && p.following.Length > 0)) {
                 Unfollow(p, data, stealth);
             } else {
                 Follow(p, name, data, stealth);
             }
         }
-        
+
         static void Unfollow(Player p, CommandData data, bool stealth) {
             Player who = PlayerInfo.FindExact(p.following);
             p.Message("Stopped following ", who == null ? p.following : who.ColoredName);
             p.following = "";
             if (who != null) Entities.Spawn(p, who);
-            
+
             if (!p.hidden) return;
             if (!stealth) {
                 Command.Find("Hide").Use(p, "", data);
@@ -59,15 +58,16 @@ namespace MCGalaxy.Commands.Moderation {
                 p.Message("You are still hidden.");
             }
         }
-        
+
         static void Follow(Player p, string name, CommandData data, bool stealth) {
             Player who = PlayerInfo.FindMatches(p, name);
             if (who == null) return;
             if (who == p) { p.Message("Cannot follow yourself."); return; }
             if (!CheckRank(p, data, who, "follow", false)) return;
-            
-            if (who.following.Length > 0) { 
-                p.Message(who.ColoredName+ " %Sis already following " + who.following); return; 
+
+            if (who.following.Length > 0) {
+                p.Message(who.ColoredName + " %Sis already following " + who.following);
+                return;
             }
 
             if (!p.hidden) Command.Find("Hide").Use(p, "", data);
@@ -77,12 +77,12 @@ namespace MCGalaxy.Commands.Moderation {
                 Player old = PlayerInfo.FindExact(p.following);
                 if (old != null) Entities.Spawn(p, old);
             }
-            
+
             p.following = who.name;
             p.Message("Following " + who.ColoredName + "%S. Use %T/Follow %Sto stop.");
             Entities.Despawn(p, who);
         }
-        
+
         public override void Help(Player p) {
             p.Message("%T/Follow [name]");
             p.Message("%HFollows <name> until the command is cancelled");
