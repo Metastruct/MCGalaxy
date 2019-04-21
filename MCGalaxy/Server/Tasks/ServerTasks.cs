f203029d3f4fbf3233f65c4b47f216717c443fc1
SpiralP <spiral.palmer@gmail.com>
2019-04-21 16:37:49 -0700

    formatting

--- MCGalaxy/Server/Tasks/ServerTasks.cs
+++ MCGalaxy/Server/Tasks/ServerTasks.cs
@@ -1,15 +1,15 @@
 ﻿/*
     Copyright 2010 MCSharp team (Modified for use with MCZall/MCLawl/MCGalaxy)
     Copyright 2011 MCForge
-    
+
     Dual-licensed under the Educational Community License, Version 2.0 and
     the GNU General Public License, Version 3 (the "Licenses"); you may
     not use this file except in compliance with the Licenses. You may
     obtain a copy of the Licenses at
-    
+
     http://www.opensource.org/licenses/ecl2.php
     http://www.gnu.org/licenses/gpl-3.0.html
-    
+
     Unless required by applicable law or agreed to in writing,
     software distributed under the Licenses are distributed on an "AS IS"
     BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
@@ -21,8 +21,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Threading;
 using MCGalaxy.Commands.Chatting;
-using MCGalaxy.Network;
 using MCGalaxy.Maths;
+using MCGalaxy.Network;
 
 namespace MCGalaxy.Tasks {
     internal static class ServerTasks {
@@ -32,14 +32,14 @@ namespace MCGalaxy.Tasks {
             Server.Background.QueueRepeat(AutoSave, 1, Server.Config.BackupInterval);
             Server.Background.QueueRepeat(BlockUpdates, null, Server.Config.BlockDBSaveInterval);
         }
-        
-        
+
+
         internal static void TickPlayers(SchedulerTask task) {
             Player[] players = PlayerInfo.Online.Items;
             players = PlayerInfo.Online.Items;
             int delay = players.Length == 0 ? 100 : 20;
             task.Delay = TimeSpan.FromMilliseconds(delay);
-            
+
             for (int i = 0; i < players.Length; i++) {
                 try {
                     TickPlayer(players[i]);
@@ -48,7 +48,7 @@ namespace MCGalaxy.Tasks {
                 }
             }
         }
-        
+
         static void TickPlayer(Player p) {
             if (p.following.Length > 0) {
                 Player who = PlayerInfo.FindExact(p.following);
@@ -60,34 +60,34 @@ namespace MCGalaxy.Tasks {
                         who.possess = "";
                     return;
                 }
-                
+
                 p.SendPos(Entities.SelfID, who.Pos, who.Rot);
             } else if (p.possess.Length > 0) {
                 Player who = PlayerInfo.FindExact(p.possess);
                 if (who == null || who.level != p.level)
                     p.possess = "";
             }
-            
+
             SchedulerTask[] tasks = p.CriticalTasks.Items;
             DateTime now = DateTime.UtcNow;
             for (int i = 0; i < tasks.Length; i++) {
                 SchedulerTask task = tasks[i];
                 if (now < task.NextRun) continue;
-                
+
                 task.Callback(task);
                 task.NextRun = now.Add(task.Delay);
-                
+
                 if (task.Repeating) continue;
                 p.CriticalTasks.Remove(task);
             }
         }
-        
+
         internal static void UpdateEntityPositions(SchedulerTask task) {
             Entities.GlobalUpdate();
             PlayerBot.GlobalUpdatePosition();
             task.Delay = TimeSpan.FromMilliseconds(Server.Config.PositionUpdateInterval);
         }
-        
+
         internal static void CheckState(SchedulerTask task) {
             Player[] players = PlayerInfo.Online.Items;
             foreach (Player p in players) {
@@ -96,14 +96,14 @@ namespace MCGalaxy.Tasks {
                 } else {
                     p.Send(Packet.Ping());
                 }
-                
+
                 if (Server.Config.AutoAfkTime.Ticks <= 0) return;
                 if (DateTime.UtcNow < p.AFKCooldown) return;
-                
+
                 if (p.IsAfk) {
                     TimeSpan time = p.group.AfkKickTime;
                     if (p.AutoAfk) time += Server.Config.AutoAfkTime;
-                    
+
                     if (p.group.AfkKicked && p.LastAction.Add(time) < DateTime.UtcNow) {
                         string afkTime = p.group.AfkKickTime.Shorten(true, true);
                         p.Leave("Auto-kick, AFK for " + afkTime);
@@ -119,11 +119,11 @@ namespace MCGalaxy.Tasks {
                 }
             }
         }
-        
+
         internal static void BlockUpdates(SchedulerTask task) {
             Level[] loaded = LevelInfo.Loaded.Items;
             task.Delay = Server.Config.BlockDBSaveInterval;
-            
+
             foreach (Level lvl in loaded) {
                 try {
                     if (!lvl.SaveChanges) continue;
@@ -133,18 +133,18 @@ namespace MCGalaxy.Tasks {
                 }
             }
         }
-        
+
         internal static void AutoSave(SchedulerTask task) {
-            int count = (int)task.State;
+            int count = (int) task.State;
             count--;
             Level[] levels = LevelInfo.Loaded.Items;
-            
+
             foreach (Level lvl in levels) {
                 try {
                     if (!lvl.Changed || !lvl.SaveChanges) continue;
 
                     lvl.Save();
-                    if (count == 0)  {
+                    if (count == 0) {
                         string backup = lvl.Backup();
                         if (backup != null) {
                             lvl.Message("Backup " + backup + " saved.");
@@ -166,7 +166,7 @@ namespace MCGalaxy.Tasks {
             } catch (Exception ex) {
                 Logger.LogError("Error auto-saving players", ex);
             }
-            
+
             players = PlayerInfo.Online.Items;
             if (players.Length <= 0) return;
             string all = players.Join(p => p.name);
