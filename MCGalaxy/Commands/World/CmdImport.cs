859c83ad1c7bb6406b6b76996b35a7e6738d7ef4
SpiralP <spiral.palmer@gmail.com>
2019-04-30 00:04:22 -0700

    formatting

--- MCGalaxy/Commands/World/CmdImport.cs
+++ MCGalaxy/Commands/World/CmdImport.cs
@@ -1,14 +1,14 @@
 /*
     Copyright 2011 MCForge
-        
+
     Dual-licensed under the Educational Community License, Version 2.0 and
     the GNU General Public License, Version 3 (the "Licenses"); you may
     not use this file except in compliance with the Licenses. You may
     obtain a copy of the Licenses at
-    
+
     http://www.opensource.org/licenses/ecl2.php
     http://www.gnu.org/licenses/gpl-3.0.html
-    
+
     Unless required by applicable law or agreed to in writing,
     software distributed under the Licenses are distributed on an "AS IS"
     BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
@@ -31,7 +31,7 @@ namespace MCGalaxy.Commands.World {
             if (!Directory.Exists(Paths.ImportsDir)) {
                 Directory.CreateDirectory(Paths.ImportsDir);
             }
-            
+
             if (message.CaselessEq("all")) {
                 string[] maps = Directory.GetFiles(Paths.ImportsDir);
                 foreach (string map in maps) { ImportFile(p, map); }
@@ -42,25 +42,26 @@ namespace MCGalaxy.Commands.World {
                 ImportFile(p, message);
             }
         }
-        
+
         static void ImportWeb(Player p, string url) {
             HttpUtil.FilterURL(ref url);
             byte[] data = HttpUtil.DownloadData(url, p);
             if (data == null) return;
-            
+
             // if data is not NULL, URL must be valid
             string path = new Uri(url).AbsolutePath;
-            string map  = Path.GetFileNameWithoutExtension(path);
+            string map = Path.GetFileNameWithoutExtension(path);
             if (!Formatter.ValidMapName(p, map)) return;
-            
+
             foreach (IMapImporter imp in IMapImporter.Formats) {
                 if (!path.CaselessEnds(imp.Extension)) continue;
-                
-                using (Stream src = new MemoryStream(data)) {
-                    Import(p, imp, src, map); return;
+
+                using(Stream src = new MemoryStream(data)) {
+                    Import(p, imp, src, map);
+                    return;
                 }
             }
-            
+
             string formats = IMapImporter.Formats.Join(imp => imp.Extension);
             p.Message("%WOnly {0} formats are supported.", formats);
         }
@@ -68,27 +69,28 @@ namespace MCGalaxy.Commands.World {
         static void ImportFile(Player p, string map) {
             map = Path.GetFileNameWithoutExtension(map);
             string path = Paths.ImportsDir + map;
-            
+
             foreach (IMapImporter imp in IMapImporter.Formats) {
                 path = Path.ChangeExtension(path, imp.Extension);
                 if (!File.Exists(path)) continue;
-                
-                using (Stream src = File.OpenRead(path)) {
-                    Import(p, imp, src, map); return;
+
+                using(Stream src = File.OpenRead(path)) {
+                    Import(p, imp, src, map);
+                    return;
                 }
             }
-            
+
             string formats = IMapImporter.Formats.Join(imp => imp.Extension);
             p.Message("%WNo {0} file with that name was found in /extra/import folder.", formats);
         }
-        
+
         static void Import(Player p, IMapImporter importer, Stream src, string map) {
             if (LevelInfo.MapExists(map)) {
                 p.Message("%WMap {0} already exists. Rename the file to something else before importing",
-                          Path.GetFileNameWithoutExtension(map));
+                    Path.GetFileNameWithoutExtension(map));
                 return;
             }
-            
+
             try {
                 Level lvl = importer.Read(src, map, true);
                 try {
@@ -104,16 +106,15 @@ namespace MCGalaxy.Commands.World {
             }
             p.Message("Successfully imported map {0}!", map);
         }
-        
+
         public override void Help(Player p) {
             p.Message("%T/Import all");
             p.Message("%HImports every map in /extra/import/ folder");
             p.Message("%T/Import [url/filename]");
             p.Message("%HImports a map from a webpage or the /extra/import/ folder");
             p.Message("%HSee %T/Help Import formats %Hfor supported formats");
-            
         }
-        
+
         public override void Help(Player p, string message) {
             if (message.CaselessEq("formats")) {
                 p.Message("%HSupported formats:");
